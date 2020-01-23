852538c3cbb93e57d2fe39468fbd89cb14c8b723
Meet Soni <meet.soni@numerator.com>
2020-01-23 17:45:42 +0530

    getting value of  url and term in same page

--- WAT/WAT1_0/views.py
+++ WAT/WAT1_0/views.py
@@ -5,8 +5,19 @@ from django.http import HttpResponse
 
 def index(request):
     context = dict()
-    if "submit" in request.POST:
-        context.update({'post_output': request.POST.get('URL','')})
+    term = None
+    if request.GET.get('URL'):
+        url = request.GET.get('URL')
+        radio_value = request.GET.get('searchRadio')
+        if radio_value:
+            term = request.GET.get('term')
+        radio_value1 = request.GET.get('productRadio')
+        context.update({
+            'url' : url,
+            'IsSearch' : radio_value,
+            'ISRadio' : radio_value1,
+            'term' : term
+        })
     return render(request,"index.html",context)
 
 def test(request):
