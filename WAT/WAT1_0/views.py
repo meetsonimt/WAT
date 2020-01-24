7ebe58a3585955d8a2b91e56dc097cb96727e797
Megha Patel <megha.patel@numerator.com>
2020-01-24 20:20:35 +0530

    Added product request in View.py.

--- WAT/WAT1_0/views.py
+++ WAT/WAT1_0/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from WAT1_0.utilities.HAR_processor import HttpTrafficHAR
 from WAT1_0.utilities.search_request_match import SerachData
+from WAT1_0.utilities.product_request_match import ProductData
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,24 +12,37 @@ def index(request):
     context = dict()
     json_data = dict()
     term = None
+    
     if request.GET.get('URL'):
         url = request.GET.get('URL')
         radio_value = request.GET.get('inlineDefaultRadiosExample')
         if radio_value == "searchRadio":
             term = request.GET.get('term')
-        radio_value1 = request.GET.get('inlineDefaultRadiosExample')
-        context.update({
-            'url' : url,
-            'IsSearch' : radio_value,
-            'ISMonitor' : radio_value1,
-            'term' : term,
-            'radio_value' : radio_value
-        })
-        obj = SerachData()
-        data = obj.get_HAR(url,term)
+            # radio_value1 = request.GET.get('inlineDefaultRadiosExample')
+            context.update({
+                'url' : url,
+                'IsSearch' : radio_value,
+                #'ISMonitor' : radio_value1,
+                'term' : term,
+                'radio_value' : radio_value
+              })  
+            obj = SerachData()
+            data = obj.get_HAR(url,term)  
+            json_load = obj.searching_filter_records(data,term)          
+        elif radio_value == "productRadio":
+            context.update({
+                'url' : url,
+                #'IsSearch' : radio_value1,
+                'ISMonitor' : radio_value,               
+                'radio_value' : radio_value
+              })   
+            obj = ProductData()
+            data = obj.get_HAR(url)
+            json_load = obj.product_filter_records(data,url) 
+        
     #pprint.pprint(obj.searching_filter_records(data,"usb"))
     
-        json_load = obj.searching_filter_records(data,term)
+        #json_load = obj.searching_filter_records(data,term)
         dump_josn = json.dumps(json_load)
         json_data = json.loads(dump_josn)
     return render(request,"index.html",{'josn_data':json_data})
