7fe70120dd0c3345fbfa074013a9081e3f7b3027
Dipakkumar Rohit <dipakkumar.rohit@numerator.com>
2020-01-24 16:59:03 +0530

    copy clip

--- WAT/WAT1_0/static/scripts/custom.js
+++ WAT/WAT1_0/static/scripts/custom.js
@@ -1,27 +1,42 @@
 $(document).ready(function () {
-  $("#proxyCheck").click(function () {
-      $(".proxy").toggle(500);
-  });
+    $("#proxyCheck").click(function () {
+        $(".proxy").toggle(500);
+    });
+   
+    $('#searchRadio').on('change', function () {
+        if ($(this).is(':checked')) {
+            $("#term").show();
+        }
+    });
+    $('#productRadio').on('change', function () {
+        if ($(this).is(':checked')) {
+            $("#term").hide();
+        }
+    });
+    $(".copyToClip").click(function() {
+        var $row = $(this).closest("tr");    // Find the row
+        var $text = $row.find(".har").text(); // Find the text
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($text).select();
+        document.execCommand("copy");
+        $temp.remove();
+    });
+    $("#table tbody tr td .jsonContent").JSONView();
 
-  $('#searchRadio').on('change', function () {
-      if ($(this).is(':checked')) {
-          $("#term").show();
-      }
-  });
-  $('#productRadio').on('change', function () {
-      if ($(this).is(':checked')) {
-          $("#term").hide();
-      }
-  });
 });
 $(document).ready(function () {
-  $(".proxy").hide();
-  $("#term").hide();
+    $(".proxy").hide();
+    $("#term").hide();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#table').DataTable({
         responsive: true
     });
-    
 });
+
+/*$('#table tbody tr td button .copyToClip').on('click', function () {
+    var data = table.row(this).data();
+    alert('You clicked on ' + data[0] + '\'s row');
+});*/
